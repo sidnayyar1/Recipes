15b3a73ccf6f97f539ad4990ce1a5f5b4141bb99
sidharth nayyar <sidnayyar1@gmail.com>
2020-08-24 19:29:33 -0400

    Initial Commit

---
+++ Recipes WatchKit Extension/RecipeRowController.swift
@@ -0,0 +1,17 @@
+    //
+//  RecipeRowController.swift
+//  Recipes WatchKit Extension
+//
+//  Created by Sidharth Nayyar on 8/24/20.
+//  Copyright © 2020 Ray Wenderlich. All rights reserved.
+//
+
+import WatchKit
+
+class RecipeRowController: NSObject {
+
+    
+    @IBOutlet var titleLabel: WKInterfaceLabel!
+    
+    @IBOutlet var ingredientsLabel: WKInterfaceLabel!
+    }
